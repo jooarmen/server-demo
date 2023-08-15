947f1dd717545a0723ab04bf54203effd812404b
jooarmen <126363143+jooarmen@users.noreply.github.com>
2023-08-14 18:07:08 -0700

    added commits

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
-
+const pokemonData = require("./database.json")
 const PORT = 5000;
 const app = express();
 
-app.get('/',(req, res)=>{
+app.get('/',(req, res)=>{    
     res.send("Hello")
 });
 
-app.listen(PORT, ()=>{
+app.get('/all-pokemons',(req, res)=>{    
+    res.send(JSON.stringify(pokemonData))
+});
+
+app.listen(PORT,()=>{
     console.log(`server is running on ${PORT} successfully`)
 })
